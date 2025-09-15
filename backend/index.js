17d44dbbfa785f5ccdabe8557e5209c5c22587d8
piyawat WIRIYAYOTHIN <nicekuy7868@gmail.com>
2025-09-16 00:31:01 +0700

    routerBlog

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const bodyParser = require('body-parser');
 
 const cors = require("cors");
-
+const { router: blogRouter, DBConnect: setBlogDB } = require("./blog_api");
 
 
 const app = express();
@@ -31,99 +31,8 @@ const waitconnection = async () => {
     console.log("MySQL connected");
 };
 
-app.post('/api/create_users', async (req, res) => {
-    try{
-        const {user_id,user_name,gender} = req.body;
-        
-        const result = await wire.query(
-            'INSERT INTO users (user_id,user_name,gender) VALUES (?,?,?)',
-            [user_id, user_name, gender]
-        );
-        res.json({
-            message: 'inserted',
-            insertedId: result.insertId
-        });
-
-    } catch(error) {
-        console.error(error);
-        res.status(500).json({error : 'Database insert failed'});
-    }
-});
-
-app.get('/api/get_alluser', async(req,res) =>{
-    try{
-        let result = await wire.query('SELECT * from users'
-        )
-        res.json(result[0])
-    }catch(error) {
-        console.error(error);
-        res.status(500).json({error : 'fetch user fail'});
-    }
-
-});
-app.get('/api/note_api', async(req,res)=> {
-    try{
-        let result = await wire.query(`select n.note_id,n.message,
-            u.img,u.user_name,n.created_at
-            from note n left join users u ON n.author = u.user_id
-            ORDER BY n.note_id DESC;
-
-            `
-        )
-        res.json(result[0])
-    }catch(error) {
-        console.error(error);
-        res.status(500).json({error : 'fetch post fail'});
-    }
-});
-
-
-app.post('/api/note_api', async (req, res) => {
-  try {
-    const { user_name, message } = req.body;
-
-    if (!message) {
-      throw new Error('ไม่มี notes');
-    }
-    
-    const [userResult] = await wire.query(
-      'INSERT INTO users (user_name, gender, img) VALUES (?, ?, ?)',
-      [user_name, 'ไม่ระบุ', '/images/pfp.png']
-    );
-
-    const authorId = userResult.insertId; 
-
-    const [noteResult] = await wire.query(
-      'INSERT INTO note (message, author) VALUES (?, ?)',
-      [message, authorId]
-    );
-
-    console.log('Note added with id:', noteResult.insertId, 'author:', authorId);
-
-    res.status(201).json({
-      success: true,
-      note_id: noteResult.insertId,
-      author: authorId,
-      user_name: user_name,
-      message
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'add notes fail' });
-  }
-});
-
-
-app.delete('/api/note_api', async(req,res) => {
-    try{
-
-    }catch(error){
-        console.error(error);
-        res.status(500).json({
-            error:"can't deleted "
-        })
-    }
-})
+setBlogDB(wire);
+app.use("/api/blog", blogRouter);
 
 app.listen(8000, async () => {
     await waitconnection();
