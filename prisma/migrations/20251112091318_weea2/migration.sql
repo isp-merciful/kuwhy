65ed449da5dff0590a49d8269b7b8f5d5a7b8c25
Chitiwat Phajan <chitiwat2547@gmail.com>
2025-11-22 02:22:55 +0700

    delete migration file

--- prisma/migrations/20251112091318_weea2/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `login_name` on table `users` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE `users` MODIFY `login_name` VARCHAR(191) NOT NULL;
